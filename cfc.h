1795adac04224b81beaf57a8e3c983579c1fa38d
Somē Cho <itssomicho@gmail.com>
2025-06-16 21:51:12 +0200

    refactor

--- cfc.h
+++ cfc.h
@@ -15,32 +15,6 @@
 #ifndef CC_HEADER
 #define CC_HEADER
 
-typedef struct
-{
-  GLuint vao;
-  GLuint vbo; // float x y z
-  GLuint cbo; // float r g b a
-  GLuint ibo; // unsigned int
-  GLuint shaderProgram;
-  GLenum renderMode;
-  uint32_t *indices;
-  float *vertices;
-  float *colors;
-  size_t numIndices;
-  size_t numVertices;
-  size_t numColors;
-  float color[4];
-} ccRenderer;
-
-typedef struct
-{
-  float *vertices;
-  float *colors;
-  uint32_t *indices;
-  size_t numVertices;
-  size_t numIndices;
-} ccGeometry;
-
 #ifndef CC_GL_VERSION_MAJOR
 #define CC_GL_VERSION_MAJOR 4
 #endif
@@ -48,93 +22,233 @@ typedef struct
 #define CC_GL_VERSION_MINOR 3
 #endif
 
+//
+// UTILITIES
+//
+
+// Opens the file at `filePath` and returns the content as a `const char *`
+// owned by the caller. Returns `NULL` if this fails.
+static const char *ccReadFile(const char *filePath)
+{
+  FILE *file = fopen(filePath, "rb");
+  if (!file)
+  {
+    perror("Failed to open file");
+    return NULL;
+  }
+
+  // Seek to end to determine file size
+  fseek(file, 0, SEEK_END);
+  long fileSize = ftell(file);
+  rewind(file);
+  if (fileSize < 0)
+  {
+    perror("Failed to determine file size");
+    return NULL;
+  }
+
+  // Allocate memory (+1 for null terminator)
+  char *buffer = (char *)malloc((size_t)fileSize + 1);
+  if (!buffer)
+  {
+    perror("Failed to allocate buffer");
+    fclose(file);
+    return NULL;
+  }
+
+  // Read the file into the buffer
+  size_t bytesRead = fread(buffer, 1, (size_t)fileSize, file);
+  buffer[bytesRead] = '\0'; // Null-terminate
+
+  fclose(file);
+  return buffer;
+}
+
+// FORWARD DECLARATIONS
+
+static GLuint ccLoadDefaultShaderProgram();
+
+// WINDOW MANAGEMENT
+
 static const int CC_DEFAULT_WINDOW_WIDTH = 1200;
 static const int CC_DEFAULT_WINDOW_HEIGHT = 800;
 static int CC_CURRENT_WINDOW_WIDTH = CC_DEFAULT_WINDOW_WIDTH;
 static int CC_CURRENT_WINDOW_HEIGHT = CC_DEFAULT_WINDOW_HEIGHT;
 static GLFWwindow *CC_MAIN_WINDOW;
 
-static uint64_t CC_CURRENT_FRAMENUM = 0;
-static float CC_CURRENT_FPS = 0.0;
-
-static mat4s CC_DEFAULT_PROJECTION_MATRIX;
-static mat4s CC_DEFAULT_VIEW_MATRIX;
-static mat4s CC_DEFAULT_MODEL_MATRIX;
-
-const char *CC_DEFAULT_VERTEX_SHADER =
-    "#version 430 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec4 aCol;\n"
-    "uniform mat4 projection;\n"
-    "uniform mat4 model;\n"
-    "uniform mat4 view;\n"
-    "out vec4 vCol;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
-    "   vCol = aCol;\n"
-    "}\0";
-
-const char *CC_DEFAULT_FRAGMENT_SHADER = "#version 430 core\n"
-                                         "in vec4 vCol;\n"
-                                         "out vec4 FragColor;\n"
-                                         "void main()\n"
-                                         "{\n"
-                                         "   FragColor = vCol;\n"
-                                         "}\n\0";
-
-static ccRenderer CC_MAIN_RENDERER;
-
-void ccOnError(int error_code, const char *description)
-{
-  perror(description);
-}
-
-void ccOnFrameBufferSize(GLFWwindow *window, int width, int height)
-{
-  glViewport(0, 0, width, height);
-  CC_DEFAULT_PROJECTION_MATRIX = glms_ortho(0, width, 0, height, 0.1, 100.0);
-}
-
-void ccOnKey(GLFWwindow *window, int key, int scancode, int action, int mods)
-{
-  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-  {
-    glfwSetWindowShouldClose(window, 1);
-  }
-}
-
-int ccGetWidth()
+static inline int ccGetWidth()
 {
   return CC_CURRENT_WINDOW_WIDTH;
 }
 
-int ccGetHeight()
+static inline int ccGetHeight()
 {
   return CC_CURRENT_WINDOW_HEIGHT;
 }
 
-void ccSetWindowSize(int width, int height)
+static inline void ccSetWindowSize(int width, int height)
 {
   glfwSetWindowSize(CC_MAIN_WINDOW, width, height);
   // TODO: recenter window
 }
 
-void ccClearWindow(float r, float g, float b, float a)
+//
+// FRAME STATS
+//
+
+// Direct variable holding the current frame number
+static uint64_t CC_CURRENT_FRAMENUM = 0;
+
+// Direct variable holding the current framerate
+static double CC_CURRENT_FPS = 0.0;
+
+// @returns the current frame number
+static inline uint64_t ccGetFrameNum()
+{
+  return CC_CURRENT_FRAMENUM;
+}
+
+// @returns the current framerate
+static inline double ccGetFps()
+{
+  return CC_CURRENT_FPS;
+}
+//
+// RENDERER
+//
+
+//
+typedef struct ccRenderer
+{
+  // Vertex Array Object
+  GLuint vao;
+  // Vertex Buffer Object. Internally `GL_ARRAY_BUFFER`.
+  GLuint vbo;
+  // Color Buffer Object. Internally `GL_ARRAY_BUFFER`.
+  GLuint cbo;
+  // Index Buffer Object. Internally `GL_ELEMENTT_ARRAY_BUFFER`.
+  GLuint ibo;
+  GLuint shaderProgram;
+  GLenum renderMode;
+  uint32_t *indices;
+  float *vertices;
+  float *colors;
+  uint32_t numIndices;
+  uint32_t numVertices;
+  uint32_t numColors;
+  float color[4];
+} ccRenderer;
+
+// The main renderer used in the main loop
+static ccRenderer CC_MAIN_RENDERER;
+
+// Frees `vertices`, `colors` and `indices`, setting them to `NULL` and resets
+// `numVertices`, `numColors` and `numIndices` to `0`.
+static void ccResetRendererData(ccRenderer *renderer)
+{
+  free(renderer->vertices);
+  renderer->vertices = NULL;
+  renderer->numVertices = 0;
+  free(renderer->colors);
+  renderer->colors = NULL;
+  renderer->numColors = 0;
+  free(renderer->indices);
+  renderer->indices = NULL;
+  renderer->numIndices = 0;
+}
+
+// Initializes `renderer` by creating OpenGL resources. A vertex array is
+// generated for `vao`. A buffer is generated for `vbo`, `cbo`, `ibo`.
+// `ccResetRendererData` is called. `color` is set to white (1,1,1,1) and
+// `renderMode` is set to `GL_TRIANGLES`.
+static void ccCreateMainRenderer(ccRenderer *renderer)
+{
+  glGenVertexArrays(1, &renderer->vao);
+  renderer->shaderProgram = ccLoadDefaultShaderProgram();
+  glGenBuffers(1, &renderer->vbo);
+  glGenBuffers(1, &renderer->cbo);
+  glGenBuffers(1, &renderer->ibo);
+  ccResetRendererData(renderer);
+  for (size_t i = 0; i < 4; i++)
+  {
+    renderer->color[i] = 1.0;
+  }
+  renderer->renderMode = GL_TRIANGLES;
+}
+
+static void ccRenderData()
+{
+  glBindVertexArray(CC_MAIN_RENDERER.vao);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, CC_MAIN_RENDERER.ibo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+               (GLsizeiptr)(CC_MAIN_RENDERER.numIndices * sizeof(uint32_t)),
+               CC_MAIN_RENDERER.indices, GL_DYNAMIC_DRAW);
+
+  glBindBuffer(GL_ARRAY_BUFFER, CC_MAIN_RENDERER.vbo);
+  glBufferData(GL_ARRAY_BUFFER,
+               (GLsizeiptr)(CC_MAIN_RENDERER.numVertices * sizeof(float) * 3),
+               CC_MAIN_RENDERER.vertices, GL_DYNAMIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, CC_MAIN_RENDERER.cbo);
+  glBufferData(GL_ARRAY_BUFFER,
+               (GLsizeiptr)(CC_MAIN_RENDERER.numColors * sizeof(float) * 4),
+               CC_MAIN_RENDERER.colors, GL_DYNAMIC_DRAW);
+  glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(1);
+
+  glBindVertexArray(CC_MAIN_RENDERER.vao);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, CC_MAIN_RENDERER.ibo);
+  glDrawElements(CC_MAIN_RENDERER.renderMode,
+                 (GLsizei)CC_MAIN_RENDERER.numIndices, GL_UNSIGNED_INT, 0);
+}
+
+// Sets the render mode of the main renderer.
+static void ccSetRenderMode(GLenum mode)
+{
+  CC_MAIN_RENDERER.renderMode = mode;
+}
+
+//
+// DRAWING
+//
+
+typedef struct ccGeometry
+{
+  float *vertices;
+  float *colors;
+  uint32_t *indices;
+  uint32_t numVertices;
+  uint32_t numIndices;
+} ccGeometry;
+
+// Clears the window screen with the given color.
+// @param r,g,b,a Normalized values from 0-1.
+static void ccClearWindow(float r, float g, float b, float a)
 {
   glClearColor(r, g, b, a);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
-void ccSetColor(float r, float g, float b, float a)
+// Sets the color the main renderer will use for subsequent draw calls.
+// @param r,g,b,a Normalized values from 0-1.
+static inline void ccSetColor(float r, float g, float b, float a)
 {
   CC_MAIN_RENDERER.color[0] = r;
   CC_MAIN_RENDERER.color[1] = g;
   CC_MAIN_RENDERER.color[2] = b;
   CC_MAIN_RENDERER.color[3] = a;
 }
-void ccDrawTriangle(float x1, float y1, float z1, float x2, float y2, float z2,
-                    float x3, float y3, float z3)
+
+// Queues a triangle to be drawn to the screen.
+// @param x1,y1,z1 The first corner of the triangle.
+// @param x2,y2,z2 The second corner of the triangle.
+// @param x3,y3,z3 The third corner of the triangle.
+static inline void ccDrawTriangle(float x1, float y1, float z1, float x2,
+                                  float y2, float z2, float x3, float y3,
+                                  float z3)
 {
   size_t curVerticesSz = CC_MAIN_RENDERER.numVertices * sizeof(float) * 3;
   size_t newVerticesSz = curVerticesSz + sizeof(float) * 9;
@@ -180,7 +294,8 @@ void ccDrawTriangle(float x1, float y1, float z1, float x2, float y2, float z2,
   CC_MAIN_RENDERER.numIndices += 3;
 }
 
-void ccDrawGeometry(ccGeometry *geom)
+// Queues a geometry for drawing to the screen.
+static void ccDrawGeometry(ccGeometry *geom)
 {
   size_t vertexSz = sizeof(float) * 3;
   size_t curVerticesSz = CC_MAIN_RENDERER.numVertices * vertexSz;
@@ -219,69 +334,69 @@ void ccDrawGeometry(ccGeometry *geom)
   free(CC_MAIN_RENDERER.indices);
   CC_MAIN_RENDERER.indices = newIndices;
   CC_MAIN_RENDERER.numIndices += geom->numIndices;
-};
-
-void ccSetDrawMode(GLenum mode)
-{
-  CC_MAIN_RENDERER.renderMode = mode;
-};
-
-const char *ccReadFile(const char *filePath)
-{
-  FILE *file = fopen(filePath, "rb");
-  if (!file)
-  {
-    perror("Failed to open file");
-    return NULL;
-  }
-
-  // Seek to end to determine file size
-  fseek(file, 0, SEEK_END);
-  long fileSize = ftell(file);
-  rewind(file);
-
-  // Allocate memory (+1 for null terminator)
-  char *buffer = (char *)malloc(fileSize + 1);
-  if (!buffer)
-  {
-    perror("Failed to allocate buffer");
-    fclose(file);
-    return NULL;
-  }
-
-  // Read the file into the buffer
-  size_t bytesRead = fread(buffer, 1, fileSize, file);
-  buffer[bytesRead] = '\0'; // Null-terminate
-
-  fclose(file);
-  return buffer;
 }
 
-bool ccShaderCompileSuccess(const GLuint shader)
+//
+// SHADERS
+//
+
+// The default projection matrix passed to the default shader program.
+// Automatically changes when the GLFW callback `onFrameBufferSize` is called.
+// By default an orthographic projection matrix with near set to `0.1` and far
+// to `100.0`.
+static mat4s CC_DEFAULT_PROJECTION_MATRIX;
+
+// The default view matrix passed to the default shader program. By default a
+// translation of (0,0,-1).
+static mat4s CC_DEFAULT_VIEW_MATRIX;
+
+// The default model matrix. By default an identity matrix.
+static mat4s CC_DEFAULT_MODEL_MATRIX;
+
+// The default vertex shader used by the default shader program.
+static const char *CC_DEFAULT_VERTEX_SHADER =
+    "#version 430 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec4 aColor;\n"
+    "uniform mat4 projection;\n"
+    "uniform mat4 model;\n"
+    "uniform mat4 view;\n"
+    "out vec4 vColor;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
+    "   vColor = aColor;\n"
+    "}\0";
+
+// The default fragment shader used by the default shader program.
+static const char *CC_DEFAULT_FRAGMENT_SHADER = "#version 430 core\n"
+                                                "in vec4 vColor;\n"
+                                                "out vec4 FragColor;\n"
+                                                "void main()\n"
+                                                "{\n"
+                                                "   FragColor = vColor;\n"
+                                                "}\n\0";
+
+// @returns `true` if `shader` has compiled successfully. Otherwise `false`.
+static bool ccShaderCompileSuccess(const GLuint shader)
 {
   int success;
   glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
   return success == 1;
 }
 
-// Calls glGetShaderInfoLog and returns result
-const char *ccGetShaderInfoLog(const GLuint shader)
+// @returns the info log to `shader` as a `const char *` owned by the caller.
+static const char *ccGetShaderInfoLog(const GLuint shader)
 {
   char *buffer = (char *)malloc(512);
   glGetShaderInfoLog(shader, 512, NULL, buffer);
   return buffer;
 }
-uint64_t ccGetFrameNum()
-{
-  return CC_CURRENT_FRAMENUM;
-}
 
-float ccGetFps()
-{
-  return CC_CURRENT_FPS;
-};
-
-GLuint ccLoadShaderFromSource(const char *shaderSource, GLenum shaderType)
+// @returns a `GLuint` representing the shader compiled, given the
+// `shaderSource` string and the `shaderType`. This operation may fail.
+static GLuint ccLoadShaderFromSource(const char *shaderSource,
+                                     GLenum shaderType)
 {
   GLuint shader = glCreateShader(shaderType);
   glShaderSource(shader, 1, &shaderSource, NULL);
@@ -293,7 +408,9 @@ GLuint ccLoadShaderFromSource(const char *shaderSource, GLenum shaderType)
   return shader;
 }
 
-GLuint ccLoadShader(const char *shaderPath, GLenum shaderType)
+// @returns a `GLuint` representing the shader compiled from `shaderPath` and
+// given the `shaderType`. This operation may fail.
+static inline GLuint ccLoadShader(const char *shaderPath, GLenum shaderType)
 {
   const char *src = ccReadFile(shaderPath);
   GLuint shader = ccLoadShaderFromSource(src, shaderType);
@@ -301,7 +418,8 @@ GLuint ccLoadShader(const char *shaderPath, GLenum shaderType)
   return shader;
 }
 
-GLuint ccLoadDefaultShaderProgram()
+// Creates and returns the default shader program used by the main renderer.
+static GLuint ccLoadDefaultShaderProgram()
 {
   GLuint vs =
       ccLoadShaderFromSource(CC_DEFAULT_VERTEX_SHADER, GL_VERTEX_SHADER);
@@ -311,7 +429,6 @@ GLuint ccLoadDefaultShaderProgram()
   glAttachShader(shader, vs);
   glAttachShader(shader, fs);
   glLinkProgram(shader);
-
   glDeleteShader(vs);
   glDeleteShader(fs);
   return shader;
@@ -319,74 +436,53 @@ GLuint ccLoadDefaultShaderProgram()
 
 #ifndef CC_NO_MAIN
 
-void ccResetRendererData(ccRenderer *renderer)
+//
+// GLFW CALLBACKS
+//
+
+static void ccOnError(int error_code, const char *description)
 {
-  renderer->colors = NULL;
-  renderer->vertices = NULL;
-  renderer->indices = NULL;
-  renderer->numColors = 0;
-  renderer->numIndices = 0;
-  renderer->numVertices = 0;
+  (void)error_code;
+  perror(description);
 }
-void ccCreateMainRenderer(ccRenderer *renderer)
+
+static void ccOnFrameBufferSize(GLFWwindow *window, int width, int height)
 {
-  glGenVertexArrays(1, &renderer->vao);
-  renderer->shaderProgram = ccLoadDefaultShaderProgram();
-  glGenBuffers(1, &renderer->vbo);
-  glGenBuffers(1, &renderer->cbo);
-  glGenBuffers(1, &renderer->ibo);
-  ccResetRendererData(renderer);
-  for (size_t i = 0; i < 4; i++)
+  (void)window;
+  glViewport(0, 0, width, height);
+  CC_DEFAULT_PROJECTION_MATRIX =
+      glms_ortho(0, (float)width, 0, (float)height, 0.1f, 100.0);
+}
+
+static void ccOnKey(GLFWwindow *window, int key, int scancode, int action,
+                    int mods)
+{
+  (void)scancode;
+  (void)mods;
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
   {
-    renderer->color[i] = 1.0;
+    glfwSetWindowShouldClose(window, 1);
   }
-  renderer->renderMode = GL_TRIANGLES;
 }
 
-void ccRenderData()
-{
-  glBindVertexArray(CC_MAIN_RENDERER.vao);
-
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, CC_MAIN_RENDERER.ibo);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-               CC_MAIN_RENDERER.numIndices * sizeof(uint32_t),
-               CC_MAIN_RENDERER.indices, GL_DYNAMIC_DRAW);
-
-  glBindBuffer(GL_ARRAY_BUFFER, CC_MAIN_RENDERER.vbo);
-  glBufferData(GL_ARRAY_BUFFER,
-               CC_MAIN_RENDERER.numVertices * sizeof(float) * 3,
-               CC_MAIN_RENDERER.vertices, GL_DYNAMIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(0);
-
-  glBindBuffer(GL_ARRAY_BUFFER, CC_MAIN_RENDERER.cbo);
-  glBufferData(GL_ARRAY_BUFFER, CC_MAIN_RENDERER.numColors * sizeof(float) * 4,
-               CC_MAIN_RENDERER.colors, GL_DYNAMIC_DRAW);
-  glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(1);
-
-  glBindVertexArray(CC_MAIN_RENDERER.vao);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, CC_MAIN_RENDERER.ibo);
-  glDrawElements(CC_MAIN_RENDERER.renderMode, CC_MAIN_RENDERER.numIndices,
-                 GL_UNSIGNED_INT, 0);
-}
-
+// Must be defined by the user. Called only once after all the internal setup
+// has completed and directly before the draw loop is started.
 void setup();
 
+// Must be defined by the user. Called each frame in the application's main
+// loop.
 void loop();
 
 int main()
 {
-  // GLFW SETUP
   glfwSetErrorCallback(ccOnError);
-
   if (!glfwInit())
   {
     return -1;
   }
+
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, CC_GL_VERSION_MAJOR);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, CC_GL_VERSION_MINOR);
-
   CC_MAIN_WINDOW = glfwCreateWindow(
       CC_DEFAULT_WINDOW_WIDTH, CC_DEFAULT_WINDOW_HEIGHT, "GLFW", NULL, NULL);
   if (!CC_MAIN_WINDOW)
@@ -394,14 +490,15 @@ int main()
     glfwTerminate();
     return -1;
   }
+  glfwSetFramebufferSizeCallback(CC_MAIN_WINDOW, ccOnFrameBufferSize);
   glfwMakeContextCurrent(CC_MAIN_WINDOW);
 
-  glfwSetFramebufferSizeCallback(CC_MAIN_WINDOW, ccOnFrameBufferSize);
   glfwSetKeyCallback(CC_MAIN_WINDOW, ccOnKey);
   glViewport(0, 0, CC_DEFAULT_WINDOW_WIDTH, CC_DEFAULT_WINDOW_HEIGHT);
 
-  CC_DEFAULT_PROJECTION_MATRIX = glms_ortho(
-      0, CC_DEFAULT_WINDOW_WIDTH, 0, CC_DEFAULT_WINDOW_HEIGHT, 0.1, 100.0);
+  CC_DEFAULT_PROJECTION_MATRIX =
+      glms_ortho(0, (float)CC_DEFAULT_WINDOW_WIDTH, 0,
+                 (float)CC_DEFAULT_WINDOW_HEIGHT, 0.1f, 100.0);
   CC_DEFAULT_VIEW_MATRIX = glms_translate_make((vec3s){{0, 0, -1}});
   CC_DEFAULT_MODEL_MATRIX = glms_mat4_identity();
 
@@ -414,7 +511,7 @@ int main()
       glGetUniformLocation(CC_MAIN_RENDERER.shaderProgram, "projection");
 
   setup();
-  float prevTime = glfwGetTime();
+  double prevTime = glfwGetTime();
   while (!glfwWindowShouldClose(CC_MAIN_WINDOW))
   {
     glUniformMatrix4fv(umodel, 1, GL_FALSE,
@@ -429,7 +526,7 @@ int main()
     glfwSwapBuffers(CC_MAIN_WINDOW);
     glfwPollEvents();
     CC_CURRENT_FRAMENUM++;
-    float frameTime = glfwGetTime();
+    double frameTime = glfwGetTime();
     CC_CURRENT_FPS = 1.0 / (frameTime - prevTime);
     prevTime = frameTime;
   };
--- examples/particles.c
+++ examples/particles.c
@@ -7,13 +7,14 @@
 #define USE_PERLIN_NOISE true
 
 // Data structure to house all the data pertaining to the particle system.
-typedef struct {
+typedef struct
+{
   vec3 pos[NUM_PARTICLES];
   float heading[NUM_PARTICLES];
   float speed[NUM_PARTICLES];
   uint8_t age[NUM_PARTICLES];
   uint32_t maxAge[NUM_PARTICLES];
-  size_t numParticles;
+  uint32_t numParticles;
 } Particles;
 
 // Use white to render particles
@@ -23,7 +24,8 @@ static Particles ps;
 
 // This method converts our particle system in to a ccGeometry, which can be
 // rendered with the default renderer.
-ccGeometry createGeometryFromParticles(Particles *p) {
+ccGeometry createGeometryFromParticles(Particles *p)
+{
   // Initialize the geometry and dynamically allocate the arrays
   ccGeometry g;
   g.vertices = (float *)malloc(p->numParticles * sizeof(float) * 3 /*xyz*/);
@@ -31,11 +33,12 @@ ccGeometry createGeometryFromParticles(Particles *p) {
   g.indices = (uint32_t *)malloc(p->numParticles * sizeof(uint32_t));
 
   // Write the data from the particle system to the ccGeometry
-  for (size_t i = 0; i < p->numParticles; i++) {
+  for (size_t i = 0; i < p->numParticles; i++)
+  {
     memcpy(&g.vertices[i * 3], p->pos[i], sizeof(float) * 3);
     memcpy(&g.colors[i * 4], particleColor, sizeof(particleColor));
     // The default renderer requires indices, so each vertex gets its own index
-    g.indices[i] = i;
+    g.indices[i] = (uint32_t)i;
   }
 
   g.numVertices = p->numParticles;
@@ -46,54 +49,63 @@ ccGeometry createGeometryFromParticles(Particles *p) {
 // We convert the heading and speed to a velocity and add it to the particle's
 // position. Then we nudge the heading abit and increment the age. If the
 // particle is older than its max age, we generate new data.
-void updateParticles(Particles *p) {
-  float noisef = 0.005;
-  float t = glfwGetTime() * 0.5;
-  for (size_t i = 0; i < p->numParticles; i++) {
-    if (p->age[i] < p->maxAge[i]) {
+void updateParticles(Particles *p)
+{
+  float noisef = 0.005f;
+  float t = (float)glfwGetTime() * 0.5f;
+  for (size_t i = 0; i < p->numParticles; i++)
+  {
+    if (p->age[i] < p->maxAge[i])
+    {
       vec3 velocity = {cosf(p->heading[i]) * p->speed[i],
                        sinf(p->heading[i]) * p->speed[i], 0.0};
       glm_vec3_add(velocity, p->pos[i], p->pos[i]);
 #if USE_PERLIN_NOISE
       p->heading[i] = glm_perlin_vec3((vec3){p->pos[i][0] * noisef,
                                              p->pos[i][1] * noisef, t}) *
-                      M_PI * 2;
+                      (float)M_PI * 2.f;
 #else
       p->heading[i] += p->speed[i] * 0.01;
 #endif
       p->age[i]++;
-    } else {
-      p->pos[i][0] = ((float)rand() / (float)RAND_MAX) * ccGetWidth();
-      p->pos[i][1] = ((float)rand() / (float)RAND_MAX) * ccGetWidth();
-      p->heading[i] = ((float)rand() / (float)RAND_MAX) * M_PI * 2;
-      p->speed[i] = ((float)rand() / (float)RAND_MAX) * 2.0 + 1.0;
+    }
+    else
+    {
+      p->pos[i][0] = ((float)rand() / (float)RAND_MAX) * (float)ccGetWidth();
+      p->pos[i][1] = ((float)rand() / (float)RAND_MAX) * (float)ccGetWidth();
+      p->heading[i] = ((float)rand() / (float)RAND_MAX) * (float)M_PI * 2.f;
+      p->speed[i] = ((float)rand() / (float)RAND_MAX) * 2.f + 1.f;
       p->age[i] = 0;
-      p->maxAge[i] = rand() % 200;
+      p->maxAge[i] = (uint32_t)rand() % 200;
     }
   }
 }
 
-void setup() {
-  srand(time(NULL));
-  ccSetDrawMode(GL_POINTS);
+void setup()
+{
+  srand((uint32_t)time(NULL));
+  ccSetRenderMode(GL_POINTS);
   // Initialize the particle system
   ps.numParticles = NUM_PARTICLES;
-  for (size_t i = 0; i < NUM_PARTICLES; i++) {
-    ps.pos[i][0] = ((float)rand() / (float)RAND_MAX) * ccGetWidth();
-    ps.pos[i][1] = ((float)rand() / (float)RAND_MAX) * ccGetWidth();
+  for (size_t i = 0; i < NUM_PARTICLES; i++)
+  {
+    ps.pos[i][0] = ((float)rand() / (float)RAND_MAX) * (float)ccGetWidth();
+    ps.pos[i][1] = ((float)rand() / (float)RAND_MAX) * (float)ccGetWidth();
     ps.pos[i][2] = 0;
-    ps.heading[i] = ((float)rand() / (float)RAND_MAX) * M_PI * 2;
-    ps.speed[i] = ((float)rand() / (float)RAND_MAX) * 2.0 + 1.0;
+    ps.heading[i] = ((float)rand() / (float)RAND_MAX) * (float)M_PI * 2.f;
+    ps.speed[i] = ((float)rand() / (float)RAND_MAX) * 2.f + 1.f;
     ps.age[i] = 0;
-    ps.maxAge[i] = rand() % 500;
+    ps.maxAge[i] = (uint32_t)rand() % 500;
   }
 }
 
-void loop() {
+void loop()
+{
   ccClearWindow(0, 0, 0, 1.0);
   ccGeometry g = createGeometryFromParticles(&ps);
   ccDrawGeometry(&g);
-  if (PRINT_FPS) {
+  if (PRINT_FPS)
+  {
     printf("%f\n", ccGetFps());
   }
   free(g.vertices);
