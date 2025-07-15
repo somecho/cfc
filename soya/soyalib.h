59234446b9a32ef2cac787e863a5e73fbb3fb18f
Somē Cho <itssomicho@gmail.com>
2025-07-15 13:58:17 +0200

    remove libpng dependency

--- soya/soyalib.h
+++ soya/soyalib.h
@@ -10,11 +10,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include <cglm/cglm.h>
 #include <cglm/struct.h>
-
-#include <png.h>
 
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
@@ -254,11 +253,6 @@ static inline void syFboDraw(syApp *app, syFbo *fbo);
 //
 
 static inline const char *syReadFile(const char *filePath);
-
-static inline int syWriteFrameRGB(const char *filename, int width, int height);
-
-static inline int syWritePngRGB(const char *filename, void *data, int width,
-                                int height);
 
 ////////////////////////////////////////////////////////////
 //
@@ -901,88 +895,6 @@ static inline const char *syReadFile(const char *filePath)
 
   fclose(file);
   return buffer;
-}
-
-static inline int syWriteFrameRGB(const char *filename, int width, int height)
-{
-  int success = -1;
-  void *pixels = malloc(width * height * 3 * sizeof(int));
-  if (!pixels)
-  {
-    return success;
-  }
-  glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, pixels);
-  success = syWritePngRGB(filename, pixels, width, height);
-  free(pixels);
-  pixels = NULL;
-  return success;
-}
-
-static inline int syWritePngRGB(const char *filename, void *data, int width,
-                                int height)
-{
-  FILE *fp = fopen(filename, "wb");
-  if (!fp)
-  {
-    perror("fopen");
-    return 1;
-  }
-
-  png_structp png =
-      png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-  if (!png)
-  {
-    fclose(fp);
-    return 1;
-  }
-
-  png_infop info = png_create_info_struct(png);
-  if (!info)
-  {
-    png_destroy_write_struct(&png, NULL);
-    fclose(fp);
-    return 1;
-  }
-
-  if (setjmp(png_jmpbuf(png)))
-  {
-    png_destroy_write_struct(&png, &info);
-    fclose(fp);
-    return 1;
-  }
-
-  png_init_io(png, fp);
-
-  png_set_IHDR(png, info, width, height, 8, PNG_COLOR_TYPE_RGB,
-               PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE,
-               PNG_FILTER_TYPE_BASE);
-
-  png_write_info(png, info);
-
-  png_bytep d = (png_bytep)data;
-
-  png_bytep *row_pointers = malloc(sizeof(png_bytep) * height);
-  if (!row_pointers)
-  {
-    png_destroy_write_struct(&png, &info);
-    fclose(fp);
-    return 1;
-  }
-
-  for (int y = 0; y < height; y++)
-  {
-    row_pointers[y] = d + y * width * 3;
-  }
-
-  png_write_image(png, row_pointers);
-  png_write_end(png, NULL);
-
-  // Cleanup
-  free(row_pointers);
-  png_destroy_write_struct(&png, &info);
-  fclose(fp);
-
-  return 0;
 }
 
 ////////////////////////////////////////////////////////////
