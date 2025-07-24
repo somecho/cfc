ad182ee21a72b1a824597e6f45f3464d67f771ad
Sam Tsao <samtsao@gmx.de>
2025-07-24 15:04:21 +0200

    add documentation for color.h

--- soya/lib/color.h
+++ soya/lib/color.h
@@ -1,25 +1,51 @@
 /**
  * @file color.h
+ *
+ * Color-related data structures and functions. Also contains macros for
+ * predefined RGBA colors.
+ *
  * */
 
-#ifndef _SOYA_COLOR_H
-#define _SOYA_COLOR_H
+#pragma once
 
-#include <math.h>
-#include <string.h>
 #include <stdio.h>
 #include <math.h>
 
+/**
+ * A 4-component color struct. The first 3 components can be used to represent
+ * RGB or HSV. The last component represents alpha. The color values are encoded
+ * as 32-bit floats.
+ *
+ * Example usage:
+ * ```
+ * syColor white = {{1},{1},{1},{1}};
+ * syColor red = {{1},{0},{0},{1}};
+ * assert(white.r == red.r);
+ * ```
+ *
+ * @sa syRgb, syHsv
+ * @since 0.1.0
+ **/
 typedef struct syColor {
   union {
-    float r, h;
+    /** Red */
+    float r;
+    /** Hue */
+    float h;
   };
   union {
-    float g, s;
+    /** Green */
+    float g;
+    /** Saturation */
+    float s;
   };
   union {
-    float b, v;
+    /** Blue */
+    float b;
+    /** Value */
+    float v;
   };
+  /** Alpha */
   float a;
 } syColor;
 
@@ -32,15 +58,38 @@ typedef struct syColor {
 #define SY_WHITE (syColor){{1}, {1}, {1}, 1}
 #define SY_YELLOW (syColor){{1}, {1}, {0}, 1}
 
+/**
+ * Creates a @ref syColor to hold RGBA values.
+ * @param r Red. [0-1]
+ * @param g Green. [0-1]
+ * @param b Blue. [0-1]
+ * @param a Alpha. [0-1]
+ * @returns syColor
+ * @since 0.1.0
+ * */
 static inline syColor syRgb(float r, float g, float b, float a) {
   return (syColor){{r}, {g}, {b}, a};
 }
 
+/**
+ * Creates a @ref syColor to hold HSVA values.
+ * @param h Hue. [0-1]
+ * @param s Saturation. [0-1]
+ * @param v Value. [0-1]
+ * @param a Alpha. [0-1]
+ * @returns syColor
+ * @since 0.1.0
+ * */
 static inline syColor syHsv(float h, float s, float v, float a) {
   return (syColor){{h}, {s}, {v}, a};
 }
 
-/** @since 0.1.0 */
+/**
+ * @param rgb A `syColor` containg RGBA values.
+ * @returns A new HSV `syColor` converted from `rgb`
+ * @sa syHsvToRgb
+ * @since 0.1.0
+ * */
 static inline syColor syRgbToHsv(syColor rgb) {
   float fCMax = fmaxf(fmaxf(rgb.r, rgb.g), rgb.b);
   float fCMin = fminf(fminf(rgb.r, rgb.g), rgb.b);
@@ -67,7 +116,12 @@ static inline syColor syRgbToHsv(syColor rgb) {
   return out;
 }
 
-/** @since 0.2.0 */
+/**
+ * @param rgb A `syColor` containg HSVA values.
+ * @returns A new RGB `syColor` converted from `hsv`
+ * @sa syRgbToHsv
+ * @since 0.2.0
+ * */
 static inline syColor syHsvToRgb(syColor hsv) {
   float fV = hsv.v;
   float fS = hsv.s;
@@ -98,14 +152,19 @@ static inline syColor syHsvToRgb(syColor hsv) {
 }
 
 /**
- * @c color in HSV or RGB
+ * @param c A color containing either HSV or RGB values.
+ * @returns A copy of `c` with new alpha value of `a`
+ * @since 0.1.0
  * */
 static inline syColor syWithAlpha(syColor c, float a) {
   return syRgb(c.r, c.g, c.b, a);
 }
 
+/**
+ * Prints the values of the color's 4 components.
+ * @param c The color to be printed.
+ * @since 0.1.0
+ * */
 static inline void syPrintColor(syColor c) {
   printf("syColor %f %f %f %f\n", c.r, c.g, c.b, c.a);
 }
-
-#endif  // _SOYA_COLOR_H
