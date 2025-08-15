4c0c62af924bcd1e445fe48f2d0400581c449e2d
Somē Cho <itssomicho@gmail.com>
2025-08-15 10:51:53 +0200

    sySaveFrame - add stbi_flip_vertically_on_write

--- soya/extras/screenshot.h
+++ soya/extras/screenshot.h
@@ -7,6 +7,7 @@
 #include <soya/glad/glad.h>
 
 void sySaveFrame(int w, int h, char *filename) {
+  stbi_flip_vertically_on_write(1);
   void *pixels = calloc(w * h * 4, sizeof(char));
   glReadPixels(0, 0, w, h, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
   int comp = 4;  // RGBA
